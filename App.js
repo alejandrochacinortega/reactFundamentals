bc3dfc6ad8790b207143be1d0bab514e97cb9b44
omar.chacin <omar.chacin@sigmagroup.no>
2016-08-05 14:50:30 +0200

    State basics

--- App.js
+++ App.js
@@ -3,27 +3,32 @@ import ReactDOM from 'react-dom';
 
 //Class Component
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: "This is the state hey",
+      cat: 0
+    }
+  }
+
+  update(e) {
+    this.setState({
+      txt: e.target.value
+    })
+  }
+
   render() {
-    let txt = this.props.txt;
     return (
       <div>
-        <h2>Hey {txt}</h2>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h2>{this.state.txt}</h2>
       </div>
     );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-};
-
-App.defaultProps = {
-  txt: 'This is the default txt'
-};
-
 ReactDOM.render(
-  <App cat={5}/>, document.getElementById('app')
+  <App />, document.getElementById('app')
 );
 
 export default App;
