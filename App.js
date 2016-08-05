d57e20f0a5421e2636ff27ab8c010aefe90c907c
omar.chacin <omar.chacin@sigmagroup.no>
2016-08-05 14:33:11 +0200

    Class and Stateless component

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+//Class Component
 class App extends React.Component {
   render() {
     return (
-      <div>You</div>
-      )
+      <div>
+        <h2>Hey</h2>
+        <b>Bold</b>
+      </div>
+    );
   }
 }
+
+
+//Stateless function component
+// const App = () => <h1>Hello</h1>;
 
 export default App;
