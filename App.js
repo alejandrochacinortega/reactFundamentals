fbb92f8f900969324c75ccc2bc8a25fbe4b45a47
omar.chacin <omar.chacin@sigmagroup.no>
2016-08-05 14:45:04 +0200

    Adding properties

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 //Class Component
 class App extends React.Component {
   render() {
+    let txt = this.props.txt;
     return (
       <div>
-        <h2>Hey</h2>
-        <b>Bold</b>
+        <h2>Hey {txt}</h2>
       </div>
     );
   }
 }
 
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+};
 
-//Stateless function component
-// const App = () => <h1>Hello</h1>;
+App.defaultProps = {
+  txt: 'This is the default txt'
+};
+
+ReactDOM.render(
+  <App cat={5}/>, document.getElementById('app')
+);
 
 export default App;
