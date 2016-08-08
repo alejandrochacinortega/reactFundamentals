4a54d950fa7afe8d8a8fa309f78f29bf34a826a2
omar.chacin <omar.chacin@sigmagroup.no>
2016-08-08 08:02:46 +0200

    Using refs to access components added

--- App.js
+++ App.js
@@ -5,35 +5,49 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      txt: "This is the state hey",
-      update: this.update.bind(this)
-    }
+    this.state  = {
+      red  : 0,
+      blue : 0,
+      green: 0
+    };
+    this.update = this.update.bind(this)
   }
 
   update(e) {
     this.setState({
-      txt: e.target.value
+      red  : ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
+      blue : ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
     })
   }
 
   render() {
     return (
       <div>
-        <Widget txt={this.state.txt} update={this.state.update}/>
+        {this.state.txt}
+        <Slider ref="red" update={this.update}/>
+        {this.state.red}
+        <br/>
+        <Slider ref="blue" update={this.update}/>
+        {this.state.blue}
+        <br/>
+        <Slider ref="green" update={this.update}/>
+        {this.state.green}
+        <br/>
       </div>
     );
   }
 }
 
-const Widget = (props) => {
-  return (
-    <div>
-      <input type="text" onChange={props.update} />
-      <h2>{props.txt}</h2>
-    </div>
-  )
-};
+class Slider extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="inp" type="range" min="0" max="255" onChange={this.props.update}/>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <App />, document.getElementById('app')
